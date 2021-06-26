03c24a9b98eae93a4ce47fe035403591edbd53e9
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-26 11:22:34 -0700

    Added submit function for search form

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // Submit function that gets the form from the html and routes to the search fields input value
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
   }
-
 }
