0ab7b27713efe1b52acc335019c28f400450b812
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-26 14:14:51 -0700

    Updated environment variables:

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,11 +2,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
+
   constructor(private http: HttpClient) {}
 
   getGameList(
